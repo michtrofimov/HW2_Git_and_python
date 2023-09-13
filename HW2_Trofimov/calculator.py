fba1af87a2b1c371cdb43443df765d8de85c7be4
Michil Trofimov <trofimov.michil@gmail.com>
2023-09-13 17:19:08 +0300

    Add main function

---
+++ HW2_Trofimov/calculator.py
@@ -0,0 +1,25 @@
+calc_input = input()
+
+
+def main(query_for_calc):
+    calc_input_lst = calc_input.split()
+    operation = calc_input[1]
+    number_1 = calc_input[0]
+    number_2 = calc_input[2]
+    if operation == "+":
+        res = addition(number_1, number_2)
+    if operation == "-":
+        # res = subtraction(number_1, number_2)
+        pass
+    if operation == "*":
+        # res = multiplication(number_1, number_2)
+        pass
+    if operation == "/":
+        # res = division(number_1, number_2)
+        pass
+    return res
+
+
+def addition(n1, n2):
+    res = n1 + n2
+    return res
